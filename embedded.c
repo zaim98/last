000c91eaeb3b95c874f5edd8b39aa428a55893e0
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-08-23 19:46:45 +0000

    new file

---
+++ embedded.c
@@ -0,0 +1,98 @@
+#include "shell.h"
+/**
+ * my_exit - Exits the shell.
+ * @info: Structure containing potential arguments.
+ * Description: Exits the shell with a given exit status,
+ *              or returns 0 if info.argv[0] != "exit".
+ * Return: Exits with the provided exit status or 0.
+ */
+int my_exit(info_t *info)
+{
+	int exit_check;
+
+	if (info->argv[1])
+	{
+		exit_check = _erratoi(info->argv[1]);
+		if (exit_check == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * my_cd - Changes the current directory of the process.
+ * @info: Structure containing potential arguments.
+ * Description: Changes the current directory of the process.
+ * Return: Always 0.
+ */
+int my_cd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret =
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = 
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * my_help - Displays help information.
+ * @info: Structure containing potential arguments.
+ *
+ * Description: Displays help information. (Function not yet implemented)
+ *
+ * Return: Always 0.
+ */
+int my_help(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
